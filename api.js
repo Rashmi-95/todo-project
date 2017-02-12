9d7da61c50f7c4e78cc9ee322f3d749885ad85f7
rashmi-95 <rashmi05rash@gmail.com>
2017-02-12 11:42:37 +0530

    almost complete

--- api.js
+++ api.js
@@ -14,11 +14,14 @@ app.use(express.static('public'))
 const readFromDB = require('./readDb.js')
 const writeToDB = require('./writeDb.js')
 const UpdateDB = require('./updateDb.js')
-const deleteFromDB = require('./deleteDb')
+const deleteFromDB = require('./deleteDb.js')
+const checkAllDB = require('./checkAllDb.js')
+const deleteCompleted = require('./deleteCompleted.js')
 
 app.get('/', function (req, res) {
   res.render('pages/index')
 })
+
 app.get('/read', function (req, res) {
   readFromDB(sequelize, res)
 })
@@ -35,9 +38,19 @@ app.put('/update/:id/', function (req, res) {
   UpdateDB(sequelize, res, description, status, id)
 })
 
+app.put('/update/', function (req, res) {
+  const checkall = req.body.checkAll
+  console.log(checkall)
+  checkAllDB(sequelize, res, checkall)
+})
+
 app.delete('/delete/:id', function (req, res) {
   const id = req.params.id
   deleteFromDB(sequelize, res, id)
 })
 
+app.delete('/delete/', function (req, res) {
+  const status = req.body.status
+  deleteCompleted(sequelize, res, status)
+})
 app.listen(3007)
--- public/jqueryFunctions.js
+++ public/jqueryFunctions.js
@@ -9,103 +9,235 @@ var entityMap = {
   '=': '&#x3D;'
 }
 const escapeHtml = string => String(string).replace(/[&<>"'`=\/]/g, s => entityMap[s])
+let todos = []
 
-function addItem(afterRead) {
-  const content = $('#new-todo').val()
-  $.post(`/write/${escapeHtml(content)}`, function (data) {
-    $(`#result ul`).append(`
-       <li id="${data}">
-          <input class ="checkbox" type="checkbox" name="checkbox" id="id${data}" >
-          <label for="id${data}">${content}</label>
-          <input class="editTextbox" type="text" name="editableText" style="display:none">
-          <button class="delete">X</button>
-        </li>`)
-    $('#new-todo').val('')
-    afterRead()
-  })
+function filterList () {
+  const url = location.hash
+  $('.filters a').prop('class', '')
+  switch (url) {
+    case '#/': $('a[href$="#/"').attr('class', 'selected')
+      $('.todo-list li').show()
+      break
+    case '#/active': $('a[href$="#/active"]').attr('class', 'selected')
+      $('.todo-list .active').show()
+      $('.todo-list .completed').hide()
+      break
+    case '#/completed': $('a[href$="#/completed" ]').attr('class', 'selected')
+      $('.todo-list .active').hide()
+      $('.todo-list .completed').show()
+      break
+  }
 }
-
-function updateStatus(id, status) {
+function checkall (status) {
   $.ajax({
-    url: `/update/${id}`,
+    url: `/update/`,
     type: 'PUT',
-    data: `description=&status=${status}`,
-    success: (result) => (result)
+    data: `checkAll=${status}`,
+    success: (result) => {
+      todos.forEach((item) => (item.status = status))
+      const className = (status) ? 'completed' : 'active'
+      $(`.main li`).prop('class', className)
+      showActiveCount()
+      showClearComplete()
+      filterList()
+    }
   })
 }
 
-function updateDescription(id, description) {
+function hideWhenNoList() {
+  console.log(todos, todos.empty, todos.length)
+  if (todos.empty || todos.length === 0) {
+    $('.toggle-all').hide()
+    $('.footer').hide()
+  } else {
+    $('.toggle-all').show()
+    $('.footer').show()
+  }
+}
+function deleteCompleted () {
   $.ajax({
-    url: `/update/${id}`,
-    type: 'PUT',
-    data: `description=${escapeHtml(description)}&status=`,
-    success: (result) => (result) // false case
+    url: `/delete/`,
+    type: 'DELETE',
+    data: `status=true`,
+    success: (result) => {
+      todos.forEach(function (index) {
+        if (index.status === true) {
+          delete todos[index.id]
+          $(`li#${index.id}`).remove()
+        }
+      })
+      $('.clear-completed').hide()
+      showActiveCount()
+      filterList()
+      hideWhenNoList()
+    }
   })
 }
 
-function deleteItem(id) {
+function createLi (id, description, checked = '') {
+  const className = (checked === '') ? 'active' : 'completed'
+  return `<li id="${id}" class ="${className}">
+      <div class="view">
+      <input class ="toggle" type="checkbox" id="todo-checkbox-${id}" ${checked}>
+      <label id="todo-label-${id}">${description}</label>
+      <input id="todo-edit-textbox-${id}" class="edit" type="text" name="editableText">
+      <button id="todo-button-${id}" class="destroy"></button>
+      </div>
+      
+      </li>`
+}
+
+function getActiveList () {
+  const filteredList = todos.filter((item) => {
+    return !item.status
+  })
+  return filteredList
+}
+
+function getCompletedList () {
+  const filteredList = todos.filter((item) => {
+    return item.status
+  })
+  return filteredList
+}
+
+function deleteItem (id) {
   $.ajax({
     url: `/delete/${id}`,
     type: 'DELETE',
-    success: (result) => (
-      $(`li#${id}`).remove())
+    success: (result) => {
+      delete todos[id]
+      $(`li#${id}`).remove()
+      showClearComplete()
+      showActiveCount()
+    }
   })
 }
 
-function afterRead() { // check this
-  $('#header #new-todo').keyup(function (event) {
-    if (event.keyCode === 13) {
-      addItem(afterRead)
+function updateStatus (id, status) {
+  const ItemStatus = status
+  $.ajax({
+    url: `/update/${id}`,
+    type: 'PUT',
+    data: `description=&status=${ItemStatus}`,
+    success: (result) => {
+      todos[id].status = ItemStatus
+      const className = (ItemStatus) ? 'completed' : 'active'
+      $(`li#${id}`).prop('class', className)
+      showActiveCount()
+      showClearComplete()
     }
   })
-  /* $('#write_button').click(() => addItem())*/
+}
 
-  $('.delete').click(function () {
-    console.log('delete')
+function updateDescription (id, updateDescription, originalDescription) {
+  if (updateDescription !== originalDescription) {
+    $.ajax({
+      url: `/update/${id}`,
+      type: 'PUT',
+      data: `description=${escapeHtml(updateDescription)}&status=`,
+      success: (result) => (todos[id].status = status)
+    })
+  }
+}
+
+function addItem () {
+  const content = $('.new-todo').val()
+  if (content !== '') {
+    $.post(`/write/${escapeHtml(content)}`, function (data) {
+      todos[data] = { 'id': data, 'description': content, 'status': false }
+      $('.new-todo').val('')
+      render()
+      filterList()
+      hideWhenNoList()
+    })
+  }
+}
+
+function itemFunctionality () {
+  $('.destroy').click(function () {
     deleteItem($(this).closest('li').attr('id'))
   })
 
-  $('.checkbox').change(function () {
-    console.log('checkbox');
-    (this.checked) ? updateStatus($(this).closest('li').attr('id'), true) : updateStatus($(this).closest('li').attr('id'), false)
+  $('.toggle').change(function () {
+    const id = $(this).closest('li').attr('id');
+    (this.checked) ? updateStatus(id, true) : updateStatus(id, false)
   })
 
   $('li').dblclick(function () {
-    console.log('double click')
     const value = $(this).find('label').hide().text()
-    $(this).find('.editTextbox').show().focus().val(value)//dash case for names
+    $(this).find('.destroy').hide()
+    $(this).find('.edit').show().focus().val(value)
   })
 
-  $('.editTextbox').focusout(function () {
-    console.log('edit')
-    const value = $(this).hide().val()
-    $(this).prev().html($(this).val()).show()
-    updateDescription($(this).closest('li').attr('id'), value)
+  $('.edit').focusout(function () {
+    const changedContent = $(this).hide().val()
+    if (changedContent === '') {
+      deleteItem($(this).closest('li').attr('id'))
+    } else {
+      const originalContent = $(this).prev().text()
+      $(this).prev().html($(this).val()).show()
+      updateDescription($(this).closest('li').attr('id'), changedContent, originalContent)
+    }
   })
 }
 
-function read(afterRead) {
+function listFunctionality () {
+  $('.header .new-todo').keyup(function (event) {
+    if (event.keyCode === 13) {
+      addItem()
+    }
+  })
+
+  $('.toggle-all').change(function () {
+    const status = this.checked
+    $('.toggle').prop('checked', status)
+    checkall(status)
+  })
+
+  $('.clear-completed').click(() => deleteCompleted())
+
+  $(window).on('hashchange', () => filterList())
+}
+function read () {
   $.get('/read', (data) => {
-    let content = '<ul>'
-    let checked
     data.forEach(function (item) {
-      let description = escapeHtml(item.description)
-      checked = (item.status === true) ? 'checked' : ''
-      content += `
-        <li id="${item.id}">
-          <input class ="checkbox" type="checkbox" name="checkbox" id="id${item.id}" ${checked}>
-          <label for="id${item.id}">${description}</label>
-          <input class="editTextbox" type="text" name="editableText">
-          <button class="delete">X</button>
-        </li>`
+      todos[item.id] = item
     })
-    content += '</ul>'
-    $('#result').html(content)
-    $('.editTextbox').hide()
-    afterRead()
+    render()
+    listFunctionality()
   })
 }
 
+function showActiveCount () {
+  const activeList = getActiveList()
+  $('.todo-count').text(`${activeList.length} items left`)
+}
+
+function showClearComplete () {
+  const completedList = getCompletedList();
+  (completedList.length === 0) ? $('.clear-completed').hide() : $('.clear-completed').show()
+}
+
+function render () {
+  showActiveCount()
+  showClearComplete()
+  let content = '<ul class="todo-list">'
+  let checked
+  todos.forEach(function (item) {
+    let description = escapeHtml(item.description)
+    checked = (item.status === true) ? 'checked' : ''
+    content += createLi(item.id, description, checked)
+    if (item.status === false) {
+      $('.toggle-all').prop('checked', false)
+    }
+  })
+  content += '</ul>'
+  $('.main').html(content)
+  $('.editTextbox').hide()
+  itemFunctionality()
+  hideWhenNoList()
+}
 $(document).ready(function () {
-  read(afterRead)
+  read()
 })
